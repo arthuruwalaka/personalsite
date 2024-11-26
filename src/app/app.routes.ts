763d239180d969adb56def4550ffadaf629522a5
Arthur Uwalaka <arthuruwalaka@Arthurs-MacBook-Air.local>
2024-11-26 14:06:56 -0700

    new components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'; 
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './skills/skills.component';
+import path from 'path';
 export const routes: Routes = [
     {path: 'about', component: AboutComponent},
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'projects', component: ProjectsComponent},   
+    {path: 'resume', component: ResumeComponent},
+    {path: 'skills', component: SkillsComponent}, 
+    {path: '**', redirectTo: ''}
 ];
