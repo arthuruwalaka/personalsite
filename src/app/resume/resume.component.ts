5c789a2c748e3389afbf80bab0920fdf6846a586
Arthur Uwalaka <arthuruwalaka@Arthurs-MacBook-Air.local>
2025-08-04 12:33:15 -0600

    Added drop down to resune, also implemented download pdf button

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
+  expandedJobs: boolean[] = [false, false, false];
 
+  toggleJobDetails(index: number): void {
+    this.expandedJobs[index] = !this.expandedJobs[index];
+  }
+
+  downloadResume(): void {
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = 'assets/documents/ArthurUwalaka-Resume.pdf';
+    link.download = 'ArthurUwalaka-Resume.pdf';
+    
+    // Append to body, click, and remove
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
