9e134fa6e40ffba253b6d8784c9d59ff6f409125
arthuruwalaka <55423381+arthuruwalaka@users.noreply.github.com>
2025-09-10 15:05:51 -0600

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// export const environment = {
-//   production: false,
-//   emailjsServiceId: 'service_8ajnr3i',
-//   emailjsTemplateId: 'template_k2j0sqt',
-//   emailjsPublicKey: 'Y--ToMxf9eCFJiMhO'
-// };
