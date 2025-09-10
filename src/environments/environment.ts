65fedab677a8f6fdfb4865182b0bc07d8382ddb6
Arthur Uwalaka <arthuruwalaka@Arthurs-MacBook-Air.local>
2025-09-10 15:07:51 -0600

    added enviroment.ts to git ignore and soley using production  variables now

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-// export const environment = {
-//   production: false,
-//   emailjsServiceId: 'service_8ajnr3i',
-//   emailjsTemplateId: 'template_k2j0sqt',
-//   emailjsPublicKey: 'Y--ToMxf9eCFJiMhO'
-// };
+export const environment = {
+  production: false,
+  emailjsServiceId: 'service_8ajnr3i',
+  emailjsTemplateId: 'template_k2j0sqt',
+  emailjsPublicKey: 'Y--ToMxf9eCFJiMhO'
+};
