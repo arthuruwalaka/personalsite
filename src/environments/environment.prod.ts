4eefb5f861f625f9f79be3f7714bb5b0a1daf265
Arthur Uwalaka <uwalakaarthur@gmail.com>
2025-09-14 16:19:20 -0600

    String type casting for enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  emailjsServiceId: process.env['EMAILJS_SERVICE_ID'],
-  emailjsTemplateId: process.env['EMAILJS_TEMPLATE_ID'],
-  emailjsPublicKey: process.env['EMAILJS_PUBLIC_KEY']
+  emailjsServiceId: process.env['EMAILJS_SERVICE_ID'] as string,
+  emailjsTemplateId: process.env['EMAILJS_TEMPLATE_ID'] as string,
+  emailjsPublicKey: process.env['EMAILJS_PUBLIC_KEY'] as string
 };
