04441eac77fece886f46b3b8cc8a5c90e5d66457
Arthur Uwalaka <uwalakaarthur@gmail.com>
2025-09-14 16:14:04 -0600

    Making sure enviroment variables work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  emailjsServiceId: 'service_8ajnr3i',
-  emailjsTemplateId: 'template_k2j0sqt',
-  emailjsPublicKey: 'Y--ToMxf9eCFJiMhO'
+  emailjsServiceId: 'EmailJS_SERVICE_ID',
+  emailjsTemplateId: 'EmailJS_TEMPLATE_ID',
+  emailjsPublicKey: 'EmailJS_PUBLIC_KEY'
 };
